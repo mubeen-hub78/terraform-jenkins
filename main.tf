096a7967f9589ecc8e5aeccc2d23857ce7ab8d1b
mubeen-hub78 <mubeensyed7897@outlook.com>
2025-06-10 20:50:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.0.0"
+resource "local_file" "devops" {
+  filename = "devops.txt"
+  content  = "terraform task"
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0e9bbd70d26d7cf4f"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name        = "JenkinsTerraformInstance"
-    Environment = "Dev"
-    ManagedBy   = "Jenkins"
-  }
-}
-
-output "instance_id" {
-  description = "The ID of the created EC2 instance"
-  value       = aws_instance.example.id
-}
-
-output "instance_public_ip" {
-  description = "The public IP address of the created EC2 instance"
-  value       = aws_instance.example.public_ip
+resource "random_pet" "devops" {
+  prefix    = "course"
+  separator = "."
+  length    = "1"
 }
